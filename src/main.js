bb9d3f39364e50c830fbe3f38039dd852359d494
liningrui <liningrui@baidu.com>
2018-08-15 23:26:44 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 解决移动端300ms点击延迟问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  // 实际上是router: router
   router,
+  // App是一个局部组件
   components: { App },
   template: '<App/>'
 })
+
+// 路由根据网址的不同，返回不同的内容给用户
