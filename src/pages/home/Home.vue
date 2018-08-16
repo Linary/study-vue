b296aebafeaab2801fbeb0a2eeb15685376fc30e
liningrui <liningrui@baidu.com>
2018-08-16 23:08:22 +0800

    add icons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,19 +3,22 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
-    <div>test</div>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+
 export default {
   name: 'Home',
   components: {
     // 自动关联上上面的home-header
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
