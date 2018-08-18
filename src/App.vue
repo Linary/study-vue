cbe937a52d22d535b6a91db65c6faf942a37d5d6
liningrui <liningrui@baidu.com>
2018-08-19 00:47:29 +0800

    finish page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <!-- 这个一个单文件组件，放置的是vue的一个组件，其效果类似与class的定义 -->
 <template>
   <div id="app">
-    <!-- 显示的是当前路由地址所对应的内容 -->
-    <router-view/>
+    <!--路由中的内容加载过一次后就放到内存，以后不会每次都去发送ajax请求-->
+    <keep-alive>
+      <!-- 显示的是当前路由地址所对应的内容 -->
+      <router-view/>
+    </keep-alive>
   </div>
 </template>
 
