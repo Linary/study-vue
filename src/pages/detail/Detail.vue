d41a6f1dc8663c0d748afd57f9653326069045e7
liningrui <liningrui@baidu.com>
2018-08-19 16:31:35 +0800

    header animation

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
@@ -17,4 +21,6 @@ export default {
 <!--scoped 表示这些样式只对当前组件有效-->
 <style lang="stylus" scoped>
   @import "~styles/varible.styl"
+  .content
+    height 50rem
 </style>
